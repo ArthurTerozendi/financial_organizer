f7e17776fe7ad1217ef3a054ec8e2efb18c7a39f
ArthurTerozendi <arthurterozendi@gmail.com>
2024-03-26 19:05:47 -0300

    lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
 		"es2021": true
 	},
 	"extends": [
+    'airbnb-typescript/base',
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		'plugin:prettier/recommended'
@@ -31,5 +32,40 @@ module.exports = {
 		"@typescript-eslint"
 	],
 	"rules": {
-	}
+    'no-console': ['error'],
+    'no-debugger': ['error'],
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    'no-return-await': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'padding-line-between-statements': 'off',
+    '@typescript-eslint/padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+      { blankLine: 'always', prev: 'block-like', next: '*' },
+      { blankLine: 'always', prev: 'multiline-block-like', next: '*' },
+      { blankLine: 'always', prev: 'multiline-expression', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+      { blankLine: 'always', prev: '*', next: 'class' },
+      { blankLine: 'always', prev: '*', next: 'case' },
+      { blankLine: 'always', prev: '*', next: 'default' },
+      { blankLine: 'always', prev: '*', next: 'expression' },
+      { blankLine: 'any', prev: 'expression', next: 'expression' },
+      { blankLine: 'always', prev: '*', next: 'debugger' },
+      { blankLine: 'always', prev: 'debugger', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'import' },
+      { blankLine: 'any', prev: 'import', next: 'import' },
+      { blankLine: 'always', prev: '*', next: 'export' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'always', prev: '*', next: ['const', 'let', 'var'] },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      { blankLine: 'always', prev: 'directive', next: '*' },
+      { blankLine: 'any', prev: 'directive', next: 'directive' },
+    ],
+    'max-params': ['error', 5],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'import/no-unassigned-import': ['error', { allow: ['reflect-metadata', 'isomorphic-unfetch'] }],
+    'import/no-unused-modules': [1, { unusedExports: true }],
+  },
 }
