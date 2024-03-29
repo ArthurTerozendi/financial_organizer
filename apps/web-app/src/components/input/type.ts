75da815e31310a9d02f2ac4f3ca4093d0ead001e
ArthurTerozendi <arthurterozendi@gmail.com>
2024-03-29 16:38:24 -0300

    input component

---
+++ apps/web-app/src/components/input/type.ts
@@ -0,0 +1,9 @@
+import { ChangeEvent, HTMLInputTypeAttribute } from "react";
+
+export interface InputProps {
+  label: string;
+  type: HTMLInputTypeAttribute;
+  value: string | number | undefined;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+}
