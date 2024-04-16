31aec457a0e1fa24333dd5ff4925383b555e8312
ArthurTerozendi <arthurterozendi@gmail.com>
2024-04-15 21:26:57 -0300

    prisma client

---
+++ packages/db/index.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var _prismaClient_: PrismaClient;
+}
+
+function getPrismaClient() {
+  if (!global._prismaClient_) {
+    global._prismaClient_ = new PrismaClient();
+  }
+
+  return global._prismaClient_;
+}
+
+export class Db {
+  private static self: Db;
+
+  static get instance() {
+    if (!this.self) {
+      this.self = new Db();
+    }
+
+    return this.self.ref;
+  }
+  
+  private constructor(private ref: PrismaClient = getPrismaClient()) {}
+}
+
+
+export namespace Db {}
