10d2cba011994eba5e3e3b4cea2e769e2327ff7d
ArthurTerozendi <arthurterozendi@gmail.com>
2024-04-25 22:54:03 -0300

    login route

---
+++ apps/backend/src/routes/login/index.ts
@@ -0,0 +1,48 @@
+import { Db } from "@financial-organizer/db";
+import { FastifyInstance } from "fastify";
+import bcrypt from 'bcrypt';
+import jsonwebtoken from "jsonwebtoken";
+
+type LoginBody = {
+  password: string,
+  email: string,
+}
+
+const LoginSchema = {
+  password: { type: 'string' },
+  email: { type: 'string' }
+}
+
+export default async function(fastify: FastifyInstance) {
+  fastify.post('/', {
+    schema: {
+      body: LoginSchema,
+    }
+  }, async (request, reply) => {
+    const { password, email } = request.body as LoginBody;
+
+    const user = await Db.instance.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if(!user) {
+      return reply.status(404).send({ message: 'Wrong email', status: 'NOT_FOUND' });
+    }
+
+    const isRightPassword =  bcrypt.compareSync(password, user.password);
+
+    if (!isRightPassword) {
+      return reply.status(404).send({ message: 'Wrong password', status: 'BAD_REQUEST' });
+    }
+
+    const token = jsonwebtoken.sign(
+      { user: JSON.stringify(user) },
+      'private_key',
+      { expiresIn: '7d' }
+    )
+
+    reply.status(200).send({ token });
+  });
+}
