657395d907fe7a2bd08077aaf0132b53dc9c4cdd
ArthurTerozendi <arthurterozendi@gmail.com>
2024-04-29 19:54:40 -0300

    return created message and handle unique constraint for email

--- apps/backend/src/routes/signIn/index.ts
+++ apps/backend/src/routes/signIn/index.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import bcrypt from 'bcrypt';
-import { Db } from "../../../../../packages/db";
+import { Db, Prisma } from "../../../../../packages/db";
 
 type SignInBody = {
   name: string,
@@ -20,17 +20,32 @@ export default async function(fastify: FastifyInstance) {
       body: SignInSchema,
     }
   }, async (request, reply) => {
-    const { name, password, email } = request.body as SignInBody;
-    const passwordEncrypted = await bcrypt.hash(password, 10);
+    try {
+      const { name, password, email } = request.body as SignInBody;
+      const passwordEncrypted = await bcrypt.hash(password, 10);
+  
+      const newUser = await Db.instance.user.create({
+        data: {
+          email,
+          name,
+          password: passwordEncrypted,
+        }
+      })
 
-    const newUser = Db.instance.user.create({
-      data: {
-        email,
-        name,
-        password: passwordEncrypted,
+      reply.status(201).send({ message: 'New user created!' });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          return reply.status(409).send({
+            error: 'Email already in use',
+            message: 'The email you provide is already associated with another account'
+          })
+        }
       }
-    })
-
-    reply.status(200).send({ user: newUser });
+      return reply.status(500).send({
+        error: 'Internal error',
+        message: 'Unknown error'
+      });
+    }
   });
 }
