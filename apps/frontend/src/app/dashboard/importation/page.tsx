5a7706e147209c0a6dd447eed90b9d478a8a0ab5
ArthurTerozendi <arthurterozendi@gmail.com>
2024-05-25 20:46:42 -0300

    basic form structure

--- apps/frontend/src/app/dashboard/importation/page.tsx
+++ apps/frontend/src/app/dashboard/importation/page.tsx
@@ -1,8 +1,16 @@
-const Importation = () => {
+'use client'
 
+import { Input } from "@/components/input";
+
+const Importation = () => {
     return (
-      <div className="flex w-full h-full items-center justify-center">
-        Importation page
+      <div className="flex gap-8 flex-col mt-8 w-64 h-full">
+        <Input label="Descrição" value="" onChange={() => {}} type="text" />
+        <Input label="Categoria" value="" onChange={() => {}} type="text" />
+        <Input label="Valor" value="" onChange={() => {}} type="text" />
+        <Input label="Data" value="" onChange={() => {}} type="date" />
+
+        <button className="bg-purple py-1 text-white rounded font-semibold text-sm"> Criar </button>
       </div>
     );
   }
