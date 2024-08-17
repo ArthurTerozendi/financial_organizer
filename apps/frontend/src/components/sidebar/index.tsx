b8bb609d00cbfeccea4e3ae56153d0e29a2f458d
ArthurTerozendi <arthurterozendi@gmail.com>
2024-08-17 11:59:35 -0300

    change port

--- apps/frontend/src/components/sidebar/index.tsx
+++ apps/frontend/src/components/sidebar/index.tsx
@@ -10,7 +10,7 @@ const Sidebar: FC<SidebarProps> = ({ selectedPage }) => {
 
 	return (
 		<nav className="flex flex-col p-6 gap-3 bg-dark-gray w-60">
-			{displayPage.map((page) =>	<a key={`${page}_key`} className={`cursor-pointer hover:underline first:mt-10 ${selectedPage === page ? 'underline' : ''}`} style={ selectedPage === page ? { color: '#af50db', textDecorationLine: 'underline', textDecorationColor: '#af50db', textUnderlineOffset: '0.2rem', textDecorationThickness: '2px' } : {}} href={Pages[page].url} > {Pages[page].label} </a>)}
+			{displayPage.map((page) =>	<a key={`${page}_key`} className={`cursor-pointer hover:underline first:mt-10 min-w-36 ${selectedPage === page ? 'underline' : ''}`} style={ selectedPage === page ? { color: '#af50db', textDecorationLine: 'underline', textDecorationColor: '#af50db', textUnderlineOffset: '0.2rem', textDecorationThickness: '2px' } : {}} href={Pages[page].url} > {Pages[page].label} </a>)}
 		</nav>
 	);
 }
