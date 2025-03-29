407c61ef350ec7f02fcdf19251a3ce4d02dab2c5
ArthurTerozendi <arthurterozendi@gmail.com>
2025-03-29 09:42:14 -0300

    pwa

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Financial Organizer',
+        short_name: 'Financial Organizer',
+        description: 'A financial organizer application to help manage your finances',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 3000,
   },
