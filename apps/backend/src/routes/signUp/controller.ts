78b6ffc61c73fb813b9cf9d1ae647ba05dc36234
ArthurTerozendi <arthurterozendi@gmail.com>
2025-08-27 15:21:02 -0300

    create default tags

--- apps/backend/src/routes/signUp/controller.ts
+++ apps/backend/src/routes/signUp/controller.ts
@@ -3,6 +3,21 @@ import { SingUpBody } from "./schema";
 import bcrypt from "bcryptjs";
 import { Db, Prisma } from "@financial-organizer/db";
 
+async function createDefaultTags(userId: string) {
+  const tags = [
+    { name: "Alimentação", color: "#E74C3C" },
+    { name: "Transporte", color: "#FAD7A0" },
+    { name: "Lazer", color: "#82E0AA" },
+  ];
+
+  await Db.instance.tag.createMany({
+    data: tags.map((tag) => ({
+      ...tag,
+      userId,
+    })),
+  });
+}
+
 export async function singUp(
   request: FastifyRequest<{ Body: SingUpBody }>,
   reply: FastifyReply,
@@ -11,13 +26,15 @@ export async function singUp(
     const { name, password, email } = request.body as SingUpBody;
     const passwordEncrypted = await bcrypt.hash(password, 10);
 
-    await Db.instance.user.create({
+    const user = await Db.instance.user.create({
       data: {
         email,
         name,
         password: passwordEncrypted,
       },
     });
+
+    await createDefaultTags(user.id);
 
     reply.status(201).send({ message: "Novo usuário criado!" });
   } catch (error) {
